c5d30755a90a29d3cf8ae936157dcde5aad7bb95
ardaozyaman <ardaozyamanq@gmail.com>
2025-03-04 23:44:52 +0300

    no asserts

--- main.py
+++ main.py
@@ -37,57 +37,51 @@ class LCWTest(unittest.TestCase):
         self.driver.maximize_window()
 
     def test_mainCase(self):
-
         driver = self.driver
-
-        # rasgele seçilen ürün olduğu için hata alabiliriz. pantalon ve aksesuarlar için üzerinde çalışılması lazım
-        maxAttemps = 3
-        while maxAttemps > 0:
-            driver.get("https://www.lcwaikiki.com/")
-            self.wait.until(lambda driver: page_has_loaded(driver))
-            time.sleep(1)
-
+        maxAttempts = 3
+        while maxAttempts > 1:
             try:
-                # tüm işlemleri fonksiyonlar içinde yapmak daha iyi olurdu biliyorum ama bu ödev olduğu için böyle yaptım. üşendim bizden clean code istememiş zaten
-                self.goToProductPage(driver, byPassCookies=maxAttemps < 3)
-                time.sleep(1)
-
-                prodGrid = driver.find_element(*self.CAT_PRODUCT_GRID)
-
-                prodElements = prodGrid.find_elements(*self.CAT_PRODUCT_GRID_ITEM)
-
-                # rastgele olmasını istedim çünkü hata çıkmasını istiyorum
-                productGridElement = random.choice(prodElements)
-                # for döngüsü ile  prodElements içinden istenilen ürünü seçebiliriz
-
-                driver.execute_script(
-                    "arguments[0].scrollIntoView(true);", productGridElement
-                )
-
-                productGridElement.click()
-                time.sleep(1)
-                prodInfoElement = driver.find_element(*self.PROD_INFO)
-
-                sizeSelectBox = prodInfoElement.find_element(*self.SIZE_SELECT_BOX)
-                sizeSelectBox.click()
-                time.sleep(1)
-
-                addToCartButton = prodInfoElement.find_element(*self.ADD_TO_CART_BUTTON)
-                addToCartButton.click()
-                time.sleep(1)
+                self.navigateToHomepage(driver)
+                self.goToProductPage(driver, byPassCookies=maxAttempts < 3)
+                self.selectRandomProduct(driver)
+                self.addProductToCart(driver)
                 break
-
             except Exception as e:
-                maxAttemps -= 1
-                print(f"error: {e}")
-                print(f"remaining attempts: {maxAttemps}")
-                driver.get("https://www.lcwaikiki.com/")
+                maxAttempts -= 1
+                print(f"Error: {e}")
+                print(f"Remaining attempts: {maxAttempts}")
+                if maxAttempts == 0:
+                    self.fail()
                 time.sleep(1)
 
+        self.verifyCart(driver)
+
+    def navigateToHomepage(self, driver):
+        driver.get("https://www.lcwaikiki.com/")
+        self.wait.until(lambda driver: page_has_loaded(driver))
+        time.sleep(1)
+
+    def selectRandomProduct(self, driver):
+        prodGrid = driver.find_element(*self.CAT_PRODUCT_GRID)
+        prodElements = prodGrid.find_elements(*self.CAT_PRODUCT_GRID_ITEM)
+        productGridElement = random.choice(prodElements)
+        driver.execute_script("arguments[0].scrollIntoView(true);", productGridElement)
+        productGridElement.click()
+        time.sleep(1)
+
+    def addProductToCart(self, driver):
+        prodInfoElement = driver.find_element(*self.PROD_INFO)
+        sizeSelectBox = prodInfoElement.find_element(*self.SIZE_SELECT_BOX)
+        sizeSelectBox.click()
+        time.sleep(1)
+        addToCartButton = prodInfoElement.find_element(*self.ADD_TO_CART_BUTTON)
+        addToCartButton.click()
+        time.sleep(1)
+
+    def verifyCart(self, driver):
         driver.get("https://www.lcw.com/sepetim")
         self.wait.until(lambda driver: page_has_loaded(driver))
         time.sleep(2)
-
         driver.get("https://www.lcw.com")
         self.wait.until(lambda driver: page_has_loaded(driver))
         time.sleep(2)
@@ -97,28 +91,16 @@ class LCWTest(unittest.TestCase):
         time.sleep(1)
         if not byPassCookies:
             waitClick(self.wait, driver.find_element(*self.COOKIES_BUTTON)).click()
-
             waitClick(self.wait, driver.find_element(*self.OPT_IN_CLOSE)).click()
 
         navElement = driver.find_element(*self.NAV_LISTS)
-
         navElements = navElement.find_elements(*self.NAV_LIST_ITEMS)
-
-        chosenNavElements = []
-        for navElement in navElements:
-            navElementText = removeSpecChars(navElement.text)
-            for cat in self.CATEGORIES:
-                cat = removeSpecChars(cat)
-                if cat == navElementText:
-                    chosenNavElements.append(navElement)
-
+        chosenNavElements = [nav for nav in navElements if removeSpecChars(nav.text) in map(removeSpecChars, self.CATEGORIES)]
         random.choice(chosenNavElements).click()
-
         self.wait.until(lambda driver: page_has_loaded(driver))
 
     def tearDown(self):
         self.driver.quit()
-
 
 if __name__ == "__main__":
     unittest.main()
