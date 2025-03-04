3f3fca93268c3c37b1d401971ef4318a4e3aa5c3
ardaozyaman <ardaozyamanq@gmail.com>
2025-03-04 23:13:41 +0300

    -

--- main.py
+++ main.py
@@ -1,73 +1,124 @@
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import unittest
 import time
 from utils import *
+import random
+
 
 class LCWTest(unittest.TestCase):
-    
-    COOKIES_BUTTON = (By.ID,"cookieseal-banner-accept")
-    OPT_IN_CLOSE = (By.CLASS_NAME,"ins-web-opt-in-reminder-close-button")
+
+    COOKIES_BUTTON = (By.ID, "cookieseal-banner-accept")
+    OPT_IN_CLOSE = (By.CLASS_NAME, "ins-web-opt-in-reminder-close-button")
     NAV_LISTS = (By.CLASS_NAME, "menu-nav__lists")
+    NAV_LIST_ITEMS = (By.CLASS_NAME, "menu-header-item")
+    CATEGORIES = ("KADIN", "ERKEK", "KIZ ÇOCUK", "ERKEK ÇOCUK")
+
     CAT_PRODUCT_GRID = (By.CLASS_NAME, "product-grid")
-    ADD_TO_CART_BUTTON = (By.ID, "")
-    CART_BUTTON = (By.CLASS_NAME, "")
-    HOMEPAGE_LOGO = (By.CLASS_NAME, "")
-    
+    CAT_PRODUCT_GRID_ITEM = (By.CSS_SELECTOR, ".product-card.product-card--one-of-4")
+
+    PROD_INFO = (By.CSS_SELECTOR, ".row.info-panel")
+    ADD_TO_CART_BUTTON = (
+        By.CSS_SELECTOR,
+        "[class='add-to-card']",
+    )  # Sorunlu, tutarsız selector
+    SIZE_SELECT_BOX = (
+        By.CSS_SELECTOR,
+        ".option-box.option-size-box.option-size-box__stripped",
+    )
+
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(keep_alive=False)
         self.driver.implicitly_wait(10)
         self.wait = WebDriverWait(self.driver, 10)
         self.driver.maximize_window()
-        self.driver.get("https://www.lcwaikiki.com/")
 
-    def test_shopping_flow(self):
+    def test_mainCase(self):
+
         driver = self.driver
-        self.wait.until(lambda driver:page_has_loaded(driver))
-        waitClick(self.wait,driver.find_element(*self.COOKIES_BUTTON)).click()
 
-        waitClick(self.wait,driver.find_element(*self.OPT_IN_CLOSE)).click()
+        # rasgele seçilen ürün olduğu için hata alabiliriz. pantalon ve aksesuarlar için üzerinde çalışılması lazım
+        maxAttemps = 3
+        while maxAttemps > 0:
+            driver.get("https://www.lcwaikiki.com/")
+            self.wait.until(lambda driver: page_has_loaded(driver))
+            time.sleep(1)
 
-        elements = driver.find_elements(*(By.CLASS_NAME,"menu-header-item"))
-        for element in elements:
-            if element.text=="ERKEK":
-                element.click()
+            try:
+                # tüm işlemleri fonksiyonlar içinde yapmak daha iyi olurdu biliyorum ama bu ödev olduğu için böyle yaptım. üşendim bizden clean code istememiş zaten
+                self.goToProductPage(driver, byPassCookies=maxAttemps < 3)
+                time.sleep(1)
+
+                prodGrid = driver.find_element(*self.CAT_PRODUCT_GRID)
+
+                prodElements = prodGrid.find_elements(*self.CAT_PRODUCT_GRID_ITEM)
+
+                # rastgele olmasını istedim çünkü hata çıkmasını istiyorum
+                productGridElement = random.choice(prodElements)
+                # for döngüsü ile  prodElements içinden istenilen ürünü seçebiliriz
+
+                driver.execute_script(
+                    "arguments[0].scrollIntoView(true);", productGridElement
+                )
+
+                productGridElement.click()
+                time.sleep(1)
+                prodInfoElement = driver.find_element(*self.PROD_INFO)
+
+                sizeSelectBox = prodInfoElement.find_element(*self.SIZE_SELECT_BOX)
+                sizeSelectBox.click()
+                time.sleep(1)
+
+                addToCartButton = prodInfoElement.find_element(*self.ADD_TO_CART_BUTTON)
+                addToCartButton.click()
+                time.sleep(1)
                 break
-        
-        self.wait.until(lambda driver:page_has_loaded(driver))
-    
-        prodGrid = driver.find_element(*self.CAT_PRODUCT_GRID)
 
-        prodElements = prodGrid.find_elements(By.CLASS_NAME,"product-card product-card--one-of-4")
+            except Exception as e:
+                maxAttemps -= 1
+                print(f"error: {e}")
+                print(f"remaining attempts: {maxAttemps}")
+                driver.get("https://www.lcwaikiki.com/")
+                time.sleep(1)
 
-        prodElements[0].find_element(By.TAG_NAME, "a").click()
+        driver.get("https://www.lcw.com/sepetim")
+        self.wait.until(lambda driver: page_has_loaded(driver))
+        time.sleep(2)
 
-        time.sleep(5)
-        #self.assertIn("erkek", driver.current_url)
-    """    
-        driver.find_element(*self.PRODUCT_LINK).click()
+        driver.get("https://www.lcw.com")
+        self.wait.until(lambda driver: page_has_loaded(driver))
         time.sleep(2)
-        self.assertTrue("urun" in driver.current_url)
-        
-        add_to_cart_btn = driver.find_element(*self.ADD_TO_CART_BUTTON)
-        self.assertTrue(add_to_cart_btn.is_displayed())
-        add_to_cart_btn.click()
-        time.sleep(2)
-        
-        driver.find_element(*self.CART_BUTTON).click()
-        time.sleep(2)
-        self.assertIn("sepetim", driver.current_url)
-        
-        driver.find_element(*self.HOMEPAGE_LOGO).click()
-        time.sleep(2)
-        self.assertTrue("lcwaikiki.com" in driver.current_url)
 
-    """
+    def goToProductPage(self, driver, byPassCookies=False):
+        self.wait.until(lambda driver: page_has_loaded(driver))
+        time.sleep(1)
+        if not byPassCookies:
+            waitClick(self.wait, driver.find_element(*self.COOKIES_BUTTON)).click()
+
+            waitClick(self.wait, driver.find_element(*self.OPT_IN_CLOSE)).click()
+
+        navElement = driver.find_element(*self.NAV_LISTS)
+
+        navElements = navElement.find_elements(*self.NAV_LIST_ITEMS)
+
+        chosenNavElements = []
+        for navElement in navElements:
+            navElementText = removeSpecChars(navElement.text)
+            for cat in self.CATEGORIES:
+                cat = removeSpecChars(cat)
+                if cat == navElementText:
+                    chosenNavElements.append(navElement)
+
+        random.choice(chosenNavElements).click()
+
+        self.wait.until(lambda driver: page_has_loaded(driver))
+
     def tearDown(self):
         self.driver.quit()
 
 
 if __name__ == "__main__":
-    unittest.main(verbosity=1)
+    unittest.main()
--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
+
+def removeSpecChars(text):
+    cleanedText = re.sub(r'[^a-zA-Z0-9İÜÖÇĞ]', '', text)
+    return cleanedText
 
 def waitClick(wait:WebDriverWait,element):
     return wait.until(EC.element_to_be_clickable(element))
@@ -9,3 +14,28 @@ def waitClick(wait:WebDriverWait,element):
 def page_has_loaded(driver:webdriver.Chrome):
     page_state = driver.execute_script('return document.readyState;')
     return page_state == 'complete'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
